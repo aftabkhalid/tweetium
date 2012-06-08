c17801cf81395c48c6aa275087cf83715b5cdef7
aftabkhalid <aftab.khalid@7vals.com>
2012-06-08 02:54:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetium::Application.config.secret_token = '3da6b5815a5f6e7be4e4c3aeeab605220954fe14640f75503528094b2c10a26425cc6f05535d09a70e89bb87e02b5f497c46a53672292a61f2166ec763a07f53'
